4a190e7b2525da79dba7898d47be5e3ae33ff2b7
Leonardo merchan <leonardo.merchan.1992@gmail.com>
2017-04-05 03:52:32 +0000

    listo para produccion

--- db/migrate/20170323225713_create_detail_purchase_receipts.rb
+++ db/migrate/20170323225713_create_detail_purchase_receipts.rb
@@ -1,7 +1,7 @@
 class CreateDetailPurchaseReceipts < ActiveRecord::Migration
   def change
     create_table :detail_purchase_receipts do |t|
-      t.integer :cantidad
+      t.integer :cantidad, :limit => 8
       t.decimal :valor_unitario
       t.decimal :valor_final
 
