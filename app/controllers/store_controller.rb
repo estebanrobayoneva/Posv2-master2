183461c512bfad195972c9127491c77e26d413b5
estebanrobayoneva <estebanrobayoneva@gmail.com>
2017-05-20 10:34:50 -0500

    carrito acomodado con div, pagina finalizar compra pasando parametro de precio total

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
 class StoreController < ApplicationController
   def index
-    @products = Product.paginate(page: params[:page], per_page: 20)
+    @products = Product.paginate(page: params[:page], per_page: 10)
     @cart = current_cart
   end
 end
